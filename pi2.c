b8dedcb7179cca8b2f66ae4a293857a880f45590
ricardovalehrio <ricardovaleriosilvaoliveira@gmail.com>
2026-08-15 00:31:41 -0300

    Adiciona estudos de escalonamento de processos e uso do comando nice

---
+++ pi2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+double pi(long int i) {
+  long int k;
+  double p=0;
+  
+  for (k=0; k<i; k++)
+    if (k%2)
+      p -= (double) 1/(2*k+1);
+    else
+      p += (double) 1/(2*k+1);
+    return 4*p;
+}
+
+int main(void) {
+  long int i;
+  for (i=1;i<=LONG_MAX; i*=5) {
+    printf("pi(2) = %.10lf para %ld interações\n", pi(i), i);
+  }
+  return 0;
+}
